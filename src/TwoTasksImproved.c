397ebe7318731dab8bb37f7120ece6a0247f730c
Valeria Grotto <valeria.grotto01@gmail.com>
2023-10-04 11:42:51 +0000

    ex 3.1

---
+++ src/TwoTasksImproved.c
@@ -0,0 +1,169 @@
+// File: TwoTasksImproved.c
+// Author: Valeria Grotto 
+
+#include <stdio.h>
+#include "includes.h" //the file should be in the same folder
+#include <string.h>
+
+#define DEBUG 0
+
+/* Definition of Task Stacks */
+/* Stack grows from HIGH to LOW memory */
+#define   TASK_STACKSIZE       2048
+OS_STK    task1_stk[TASK_STACKSIZE];
+OS_STK    task2_stk[TASK_STACKSIZE];
+OS_STK    stat_stk[TASK_STACKSIZE];
+
+/* Definition of Task Priorities */
+#define TASK1_PRIORITY      6  // highest priority
+#define TASK2_PRIORITY      7
+#define TASK_STAT_PRIORITY 12  // lowest priority 
+
+void printStackSize(char* name, INT8U prio) 
+{
+  INT8U err;
+  OS_STK_DATA stk_data;
+    
+  err = OSTaskStkChk(prio, &stk_data);
+  if (err == OS_NO_ERR) {
+    if (DEBUG == 1)
+      printf("%s (priority %d) - Used: %d; Free: %d\n", 
+	     name, prio, stk_data.OSUsed, stk_data.OSFree);
+  }
+  else
+    {
+      if (DEBUG == 1)
+	printf("Stack Check Error!\n");    
+    }
+}
+
+/* Prints a message and sleeps for given time interval */
+void task1(void* pdata)
+{
+  OS_EVENT *sem = pdata;
+  //here pdata is the SCB
+  while (1)
+    { 
+      char text1[] = "Hello from Task1\n";
+      int i;
+
+     //use semaphore -> Wait statement
+     INT8U err;
+     OSSemPend(sem,0, &err); //as argumet I pass: - the pointer to the created sem, the timer setted to 0 -> it waits indefinitely, an error pointer 
+     for (i = 0; i < strlen(text1); i++)
+	      putchar(text1[i]);
+
+      err = OSSemPost(sem);
+      switch (err) {
+        case OS_ERR_NONE:
+           /* Semaphore signaled */
+          break;
+        case OS_ERR_SEM_OVF:
+          /* Semaphore has overflowed */
+          break; 
+      }
+      OSTimeDlyHMSM(0, 0, 0, 11); /* Context Switch to next task
+				   * Task will go to the ready state
+				   * after the specified delay
+				   */
+    }
+}
+
+/* Prints a message and sleeps for given time interval */
+void task2(void* pdata)
+{
+  OS_EVENT *sem = (OS_EVENT *)pdata;
+  while (1)
+    { 
+      char text2[] = "Hello from Task2\n";
+      int i;
+
+     INT8U  err;
+     //Wait 
+    OSSemPend(sem,0, &err); //as argumet I pass: - the pointer to the created sem, the timer setted to 0 -> it waits indefinitely, an error pointer 
+      
+    for (i = 0; i < strlen(text2); i++)
+	      putchar(text2[i]);
+      //signal
+      err = OSSemPost(sem);
+      switch (err) {
+        case OS_ERR_NONE:
+           /* Semaphore signaled */
+          break;
+        case OS_ERR_SEM_OVF:
+          /* Semaphore has overflowed */
+          break; 
+      }
+      OSTimeDlyHMSM(0, 0, 0, 4);
+      
+    }
+
+}
+
+/* Printing Statistics */
+void statisticTask(void* pdata)
+{
+  while(1)
+    {
+      printStackSize("Task1", TASK1_PRIORITY);
+      printStackSize("Task2", TASK2_PRIORITY);
+      printStackSize("StatisticTask", TASK_STAT_PRIORITY);
+    }
+}
+
+/* The main function creates two task and starts multi-tasking */
+int main(void)
+{
+
+  //Create semaphore
+   OS_EVENT *mySem = OSSemCreate(1); //this statement creates a semaphore with initial value set to 1, it returns the sem control bklock (or null if not available)
+                
+
+
+  printf("Lab 3 - Two Tasks\n");
+
+  OSTaskCreateExt
+    ( task1,                        // Pointer to task code
+      mySem,                         // Pointer to argument passed to task -> here I pass the pointer to the SCB
+      &task1_stk[TASK_STACKSIZE-1], // Pointer to top of task stack
+      TASK1_PRIORITY,               // Desired Task priority
+      TASK1_PRIORITY,               // Task ID
+      &task1_stk[0],                // Pointer to bottom of task stack
+      TASK_STACKSIZE,               // Stacksize
+      NULL,                         // Pointer to user supplied memory (not needed)
+      OS_TASK_OPT_STK_CHK |         // Stack Checking enabled 
+      OS_TASK_OPT_STK_CLR           // Stack Cleared                                 
+      );
+	   
+  OSTaskCreateExt
+    ( task2,                        // Pointer to task code
+      mySem,                         // Pointer to argument passed to task
+      &task2_stk[TASK_STACKSIZE-1], // Pointer to top of task stack
+      TASK2_PRIORITY,               // Desired Task priority
+      TASK2_PRIORITY,               // Task ID
+      &task2_stk[0],                // Pointer to bottom of task stack
+      TASK_STACKSIZE,               // Stacksize
+      NULL,                         // Pointer to user supplied memory (not needed)
+      OS_TASK_OPT_STK_CHK |         // Stack Checking enabled 
+      OS_TASK_OPT_STK_CLR           // Stack Cleared                       
+      );  
+
+  if (DEBUG == 1)
+    {
+      OSTaskCreateExt
+	( statisticTask,                // Pointer to task code
+	  NULL,                         // Pointer to argument passed to task
+	  &stat_stk[TASK_STACKSIZE-1],  // Pointer to top of task stack
+	  TASK_STAT_PRIORITY,           // Desired Task priority
+	  TASK_STAT_PRIORITY,           // Task ID
+	  &stat_stk[0],                 // Pointer to bottom of task stack
+	  TASK_STACKSIZE,               // Stacksize
+	  NULL,                         // Pointer to user supplied memory (not needed)
+	  OS_TASK_OPT_STK_CHK |         // Stack Checking enabled 
+	  OS_TASK_OPT_STK_CLR           // Stack Cleared                              
+	  );
+    }  
+
+  OSStart();
+  return 0;
+}
